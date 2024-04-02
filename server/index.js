b42d8b5667c2930d5292e38609eb7fe948abaeb1
Fiona Haria <hariafiona@gmail.com>
2024-04-02 23:49:57 +0530

    vercel deploy add

--- server/index.js
+++ server/index.js
@@ -33,6 +33,14 @@ app.use(bodyParser.urlencoded({limit: "30mb",extended: true}))
 app.use(cors())
 app.use("/assets",express.static(path.join(__dirname, 'public/assets')))
 
+app.use(cors(
+  {
+    origin: ["https://deploy-mern-1.vercel.app"],
+    methods: ["POST","GET"],
+    credentials: true
+  }
+))
+
 
 //whenever someone uploads a file, it will save in this folder
 const storage = multer.diskStorage({
